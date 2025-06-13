a071e21eec9f46617053b3daf7ac59b53de755db
TLTeechbender <myjoyhasarrived2004@gmail.com>
2025-06-13 11:17:00 +0100

    update: changed config

--- packages/gofamintui-app/sanity/sanityClient.ts
+++ packages/gofamintui-app/sanity/sanityClient.ts
@@ -1,3 +1,16 @@
+// import { createClient } from "@sanity/client";
+// import imageUrlBuilder from "@sanity/image-url";
+
+// export const sanityClient = createClient({
+//   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+//   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+//   useCdn: false,
+//   apiVersion: "2025-04-01",
+//   token: process.env.NEXT_SANITY_TOKEN!,
+// });
+
+// const builder = imageUrlBuilder(sanityClient);
+// export const urlFor = (source: string) => builder.image(source);
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
@@ -7,7 +20,8 @@ export const sanityClient = createClient({
   useCdn: false,
   apiVersion: "2025-04-01",
   token: process.env.NEXT_SANITY_TOKEN!,
+  // Use the new cache configuration
+  fetch: {
+    cache: "no-store",
+  },
 });
-
-const builder = imageUrlBuilder(sanityClient);
-export const urlFor = (source: string) => builder.image(source);
