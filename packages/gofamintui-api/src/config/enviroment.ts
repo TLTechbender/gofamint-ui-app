be945d01f789cdfd6beee6ccef24385e82651d46
TLTechbender <myjoyhasarrived2004@gmail.com>
2025-11-15 06:16:28 +0100

    fix: added in a new config

--- packages/gofamintui-api/src/config/enviroment.ts
+++ packages/gofamintui-api/src/config/enviroment.ts
@@ -8,7 +8,7 @@ const envSchema = z.object({
     PORT: z.coerce.number().default(8000),
 
     DATABASE_URL: z.string().min(1, "DATABASE_URL is required"),
-
+    EMAIL_SERVICE_URL: z.string().min(1, "EMAIL_SERVICE_URL  is required"),
     JWT_SECRET: z.string().min(32, "JWT_SECRET must be at least 32 characters"),
 
     EMAIL_HOST: z.string().min(1, "EMAIL_HOST is required"),
